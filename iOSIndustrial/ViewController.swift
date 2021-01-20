be93b2dd7e0cf644c27bf385d0fca3288d6f973a
Maxim Abakumov <maximnatural@gmail.com>
2021-01-20 16:46:07 +0300

    Initial Commit

---
+++ iOSIndustrial/ViewController.swift
@@ -0,0 +1,21 @@
+//
+// ViewController.swift
+// iOSIndustrial
+// 
+// Created by Maxim Abakumov on 2021. 01. 20.
+//
+// Copyright © 2020, Maxim Abakumov. MIT License.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
